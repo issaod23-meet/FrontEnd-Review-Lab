bd729717b250fcd3d3da3147b5f7e0d6acc494e1
issaod23-meet <109899312+issaod23-meet@users.noreply.github.com>
2022-07-25 14:04:28 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
+
+// /var x = 0 
+
+// function changeBackgroundColor(color){
+// 	document.body.style.background = color;}
+// function changeBackgroundColor1{
+// 	document.body.style.background = blue;}
+
+// function changeBackgroundColor2{
+// 	document.body.style.background = yellow;}
+
+
+// function bonus
+// {
+// 	if (x%2) {
+// 		changeBackgroundColor2
+// 		x++
+// 	}
+// 	else {
+// 		changeBackgroundColor1
+// 		x++
+// 	}
+
+// }
+// /
+
 function changeBackgroundColor(color){
-	document.body.style.background = color;
-
-}
-
+	document.body.style.background = color;}
